3bfe37caa65673d43cae1afe2cdce767c6d443a9
Piotr Poterek <peterpoterek0@gmail.com>
2024-05-07 22:51:04 +0200

    faker.js data

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const faker = require("@faker-js/faker");
 
 const app = express();
+
+app.get("/random-data", (req, res) => {
+  const randomData = {
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    address: faker.address.streetAddress(),
+    city: faker.address.city(),
+    country: faker.address.country(),
+    phone: faker.phone.phoneNumber(),
+    jobTitle: faker.name.jobTitle(),
+  };
+  res.json(randomData);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
